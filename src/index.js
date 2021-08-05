3dcff58a6161275b279e32588133e3b9c6f51847
David Salamanca <criptse@gmail.com>
2021-08-04 19:41:41 -0500

    add a user list and request from body

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const users = [];
 
 app.use(express.urlencoded({extended: false}));
 
@@ -7,4 +8,8 @@ app.listen(3000);
 
 app.use('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/users', (req, res) => {
+    users = req.body.users;
 })
